b6beb4a4cd6405788bd73b70893052edba03dbce
alwaysadevzero <119855949+alwaysadevzero@users.noreply.github.com>
2023-06-07 18:35:37 +0400

    feat: add typescript

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,15 +7,36 @@ module.exports = {
     "extends": [
         "eslint:recommended",
         'airbnb-base',
+        'airbnb-typescript/base',
+        'plugin:@typescript-eslint/recommended',
         'plugin:prettier/recommended'
     ],
+    "parser": "@typescript-eslint/parser",
     "parserOptions": {
         "ecmaVersion": 'latest',
         "sourceType": "module",
+        "project": "./tsconfig.json",
     },
-    "plugins": ["prettier"],
+    "plugins": ["prettier", "@typescript-eslint"],
     "rules": {
         'import/extensions': "off",
         "prettier/prettier": "error",
+        "@typescript-eslint/no-explicit-any": "error",
+        "@typescript-eslint/no-inferrable-types": "off",
+        "@typescript-eslint/explicit-member-accessibility": [
+            "error",
+            {
+                "accessibility": "explicit",
+                "overrides": {
+                    "accessors": "explicit",
+                    "constructors": "off",
+                    "methods": "explicit",
+                    "properties": "explicit",
+                    "parameterProperties": "explicit",
+                }
+            }
+        ],
+        "max-lines-per-function": ['error', 40],
+        '@typescript-eslint/'
     },
 };
